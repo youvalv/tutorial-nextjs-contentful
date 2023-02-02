d5e6ba3cde74b6e3dbcb2b7f450733f87822ab14
Youval Vaknin <youvalv@users.noreply.github.com>
2023-02-02 16:49:52 +0200

    initial commit

---
+++ .stackbit/cache/stackbit.config.BHWMOR2S.cjs
@@ -0,0 +1,45 @@
+function stackbit_process_cwd_shim() { return "/Users/youvalvaknin/Development/Support/testTutorial/tutorial-nextjs-contentful" }
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// stackbit.config.js
+var stackbit_config_exports = {};
+__export(stackbit_config_exports, {
+  default: () => stackbit_config_default
+});
+module.exports = __toCommonJS(stackbit_config_exports);
+var import_cms_contentful = require("../../node_modules/@stackbit/cms-contentful/dist/index.js");
+var stackbit_config_default = {
+  stackbitVersion: "~0.6.0",
+  ssgName: "nextjs",
+  nodeVersion: "16",
+  contentSources: [
+    new import_cms_contentful.ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
+    })
+  ],
+  models: {
+    page: { type: "page", urlPath: "/{slug}" }
+  }
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {});
+//# sourceMappingURL=stackbit.config.BHWMOR2S.cjs.map
---
+++ stackbit.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { ContentfulContentSource } from '@stackbit/cms-contentful'
+
+export default {
+  stackbitVersion: '~0.6.0',
+  ssgName: 'nextjs',
+  nodeVersion: '16',
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    }),
+  ],
+  models: {
+    page: { type: 'page', urlPath: '/{slug}' },
+  },
+}
